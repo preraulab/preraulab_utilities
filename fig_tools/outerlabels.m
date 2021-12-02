08a69c4448b83629f78eaa9277776947df38645c
MJP <mprerau@bwh.harvard.edu>
2021-12-02 10:13:20 -0500

    added name value pair axis locations to outerlabels.m

--- fig_tools/outerlabels.m
+++ fig_tools/outerlabels.m
@@ -4,22 +4,25 @@
 %       [h_xl, h_yl] = outerlabels(axs, xlabel_str, ylabel_str, axdir, <label options>)
 %
 %   Input:
-%   axs: vector of axes in figure
-%   x/ylabel_str: string for label text
-%   adir: 1x2 logical for x and y label locations to be reversed (default: [false false])
-%   <label options>: paired valued options for label class
+%       axs: vector of axes in figure
+%       x/ylabel_str: string for label text
+%       adir: 1x2 logical for x and y label locations to be reversed (default: [false false])
+%   Optional inputs:
+%       XAxisLocation: 'bottom' (default) or 'top'
+%       YAxisLocation: 'left' (default) or 'right'
+%       <label options>: paired valued options for label class
 %
 %   Output:
-%   h_xl: outer xlim handle
-%   h_yl: outer ylim handle
+%       h_xl: outer xlim handle
+%       h_yl: outer ylim handle
 %
 %   Example:
 %     figure
-%     ax = figdesign(3,2,'type','usletter','margins',[.05 .1 .14 .1 .05]);
+%     ax = figdesign(3,2,'type','usletter','margins',[.1 .1 .14 .1 .05]);
 %     xlabel_str = 'X Label';
 %     ylabel_str = 'Y Label';
-% 
-%     outerlabels(ax,xlabel_str, ylabel_str,[],'fontsize',18,'fontweight','bold');
+%
+%     outerlabels(ax,xlabel_str, ylabel_str,'XAxisLocation', 'top', 'fontsize',18,'fontweight','bold');
 %
 %   Copyright 2021 Michael J. Prerau, Ph.D. - http://www.sleepEEG.org
 %   This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
@@ -28,22 +31,33 @@
 %   last modified 12/01/2021
 %********************************************************************
 
-function [h_xl, h_yl] = outerlabels(ax,xlabel_str,ylabel_str, axdir, varargin)
+function [h_xl, h_yl] = outerlabels(ax,xlabel_str,ylabel_str, varargin)
 
 if nargin == 0
     figure
-ax = figdesign(3,2,'type','usletter','margins',[.05 .1 .14 .1 .05]);
-xlabel_str = 'X Label';
-ylabel_str = 'Y Label';
-end
-
-if nargin <4 || isempty(axdir)
-    axdir = [0 0];
+    ax = figdesign(3,2,'type','usletter','margins',[.05 .1 .14 .1 .05]);
+    xlabel_str = 'X Label';
+    ylabel_str = 'Y Label';
 end
 
 if isempty(varargin)
     varargin = {'FontSize', 17, 'FontWeight', 'bold'};
 end
+
+%Parse inputs to extract just the xy axis locations
+p = inputParser;
+addOptional(p,'XAxisLocation','bottom',@(x)  any(validatestring(lower(x),{'top','bottom'})));
+addOptional(p,'YAxisLocation','left',@(x) any(validatestring(lower(x),{'left','right'})));
+p.KeepUnmatched = true;
+parse(p,varargin{:});
+
+%Save results
+xloc = p.Results.XAxisLocation;
+yloc = p.Results.YAxisLocation;
+
+%Recreate varargin removing the x/y axis location name pairs
+varargin = cat(2,fieldnames(p.Unmatched),struct2cell(p.Unmatched));
+varargin = reshape(varargin',1, numel(varargin));
 
 %Force the units to be normalized
 set(gcf,'units','normalized');
@@ -64,15 +78,11 @@ axbig = axes('units','normalized','position',[bottom_x, bottom_y, width, height]
 h_xl = xlabel(xlabel_str,varargin{:});
 h_yl = ylabel(ylabel_str,varargin{:});
 
-%Hide big axess
+%Hide big axis
 axbig.Visible = 'off';
 axbig.XLabel.Visible = 'on';
 axbig.YLabel.Visible = 'on';
 
-if axdir(1)
-    axbig.XAxisLocation = "top";
-end
-
-if axdir(2)
-    axbig.YAxisLocation = "right";
-end
+%Set axis label location
+axbig.XAxisLocation = xloc;
+axbig.YAxisLocation = yloc;
