cc3ef970676fa5a946f33f3ad73efdd508d07e85
MJP <mprerau@bwh.harvard.edu>
2022-09-15 12:08:49 -0400

    fixed linkcaxes and added conv_downsample

--- linkcaxes.m
+++ linkcaxes.m
@@ -3,23 +3,25 @@ if nargin == 0
     error('Must provide axis handles');
 end
 
-%Add listeners to all axes
-for ii=1:length(ax)
-    addlistener(ax(ii),'CLim','PostSet',@(e,h)update_caxis(e,h,ax));
-end
-end
+linkprop(ax, 'CLim')
 
-%Update the clims
-function update_caxis(~, event, ax)
-%Get the clim of the selected axis
-new_clim = event.AffectedObject.CLim;
-
-%Update just the axes with different clims
-all_clims = cat(1, ax.CLim);
-inds = ~all(all_clims == repmat(new_clim,length(ax),1),2);
-
-%Update all other axes
-if any(inds)
-    set(ax(inds),'CLim',new_clim);
-end
-end
+% %Add listeners to all axes
+% for ii=1:length(ax)
+%     addlistener(ax(ii),'CLim','PostSet',@(e,h)update_caxis(e,h,ax));
+% end
+% end
+% 
+% %Update the clims
+% function update_caxis(~, event, ax)
+% %Get the clim of the selected axis
+% new_clim = event.AffectedObject.CLim;
+% 
+% %Update just the axes with different clims
+% all_clims = cat(1, ax.CLim);
+% inds = ~all(all_clims == repmat(new_clim,length(ax),1),2);
+% 
+% %Update all other axes
+% if any(inds)
+%     set(ax(inds),'CLim',new_clim);
+% end
+% end
