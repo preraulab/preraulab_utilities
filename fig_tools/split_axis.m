832ae915b93db3b105b1476aee00ff4d9f5db4ad
Michael Prerau, Ph.D <78376124+prerau@users.noreply.github.com>
2023-09-01 13:06:42 -0400

    Update split_axis.m
    Major update to split_axis that allows for interactive splitting

--- fig_tools/split_axis.m
+++ fig_tools/split_axis.m
@@ -24,6 +24,76 @@ function new_axes = split_axis(varargin)
 % Copyright 2023 Michael J. Prerau Laboratory. - http://www.sleepEEG.org
 %**************************************************************************
 
+%Run interactive mode
+if nargin <= 1
+    if nargin == 0
+        ax = gca;
+        fh = gcf;
+    else
+        ax = varargin{1};
+        assert(isa(ax,'matlab.graphics.axis.Axes'), 'Single input must be axis handle');
+        fh = get(gca,"Parent");
+    end
+
+    disp('Click to select horizontal slices, double-click/enter/escape');
+    disp('then select veritcal slices, double-click/enter/escape to complete.')
+
+    fh.UserData.y_slices = [];
+    fh.UserData.x_slices = [];
+
+    %Set range
+    xlim([0 100])
+    ylim([0 100])
+    set(ax,'units','normalized');
+
+    %Run horizontal selection
+    isHoriz = 1;
+    line_h = hline(0);
+
+    set(fh,'KeyPressFcn',@handle_keys);
+
+    % set function to call on mouse click
+    set(fh, 'WindowButtonDownFcn', {@pick_slice,  line_h, isHoriz, fh});
+    set(fh, 'WindowButtonMotionFcn', {@select_slice,ax, line_h, isHoriz});
+    uiwait(fh);
+    delete(line_h)
+
+    %Run vertical selection
+    isHoriz = 0;
+    line_h = vline(0);
+    set(fh, 'WindowButtonDownFcn', {@pick_slice,  line_h, isHoriz, fh});
+    set(fh, 'WindowButtonMotionFcn', {@select_slice, ax, line_h, isHoriz});
+    uiwait(fh)
+    disp(fh.UserData)
+    delete(line_h)
+    set(fh, 'WindowButtonMotionFcn',[]);
+    set(fh, 'WindowButtonDownFcn', []);
+
+    %Extract the slices and process them
+    x_slices = fh.UserData.x_slices;
+    y_slices = fh.UserData.y_slices;
+
+    if isempty(x_slices)
+        x_slices = 1;
+    else
+        x_slices = diff([0  unique(sort(x_slices/100)) 1]);
+        x_slices = x_slices/sum(x_slices);
+    end
+
+    if isempty(y_slices)
+        y_slices = 1;
+    else
+        y_slices = 1 - diff([0  unique(sort(y_slices/100)) 1]);
+        y_slices = y_slices/sum(y_slices);
+    end
+
+    disp('Command call:')
+    disp(regexprep(['split_axis([' num2str(y_slices) '], [' num2str(x_slices) ']);'], '\s+', ' '));
+    %Split the axes
+    split_axis(ax,y_slices,x_slices);
+    return;
+end
+
 % Parse possible axes input.
 [ax, args, ~] = axescheck(varargin{:});
 
@@ -105,3 +175,105 @@ end
 if delete_ax
     delete(ax);
 end
+
+
+function select_slice(~,~,ax, line_h, isHoriz)
+%Get the current point from the mouse
+C = get(ax, 'CurrentPoint');
+x = C(1,1);
+y = C(1,2);
+xl = xlim;
+yl = ylim;
+
+%Set key spots to lock on
+key_spots = [.05 .1 .2 .25 .3 1/3 .4 .5 .6 2/3 .7 .75 .8 .9 .95];
+%Tolerance for key spots
+tol = .01;
+
+%Contain line values
+if x<xl(1)
+    x = xl(1);
+end
+
+if x>xl(2)
+    x = xl(2);
+end
+
+if y<yl(1)
+    y = yl(1);
+end
+
+if y>yl(2)
+    y = yl(2);
+end
+
+%Get the percentage of the line
+px = (x-xl(1))/diff(xl);
+py = (y-yl(1))/diff(yl);
+
+%Find the closest hot spot
+[dxkey, closest_keyx] = min(abs(px-key_spots));
+[dykey, closest_keyy] = min(abs(py-key_spots));
+
+%Check for horizontal line
+if isHoriz
+    %Check for lock
+    if dykey<tol
+        py_close = key_spots(closest_keyy);
+        y = py_close*diff(yl)+yl(1);
+
+        line_h.LineWidth = 3;
+        line_h.Color = 'm';
+    else
+        line_h.LineWidth = 1;
+        line_h.Color = 'k';
+        py_close = py;
+    end
+    px_close = px;
+
+    line_h.YData = [y y];
+    title(ax, [num2str(py_close*100,3), '%'],'FontSize',15);
+else
+     %Check for lock
+    if dxkey<tol
+        px_close = key_spots(closest_keyx);
+        x = px_close*diff(xl)+xl(1);
+
+        line_h.LineWidth = 3;
+        line_h.Color = 'm';
+    else
+        line_h.LineWidth = 1;
+        line_h.Color = 'k';
+        px_close = px;
+    end
+
+    py_close = py;
+    line_h.XData = [x x];
+    title(ax, [num2str(px_close*100,3), '%'],'FontSize',15);
+end
+
+%Select and save the picked slice
+function pick_slice(~,~,line_h, isHoriz, fh)
+if isHoriz
+    y_val = line_h.YData(1);
+    fh.UserData.y_slices = [fh.UserData.y_slices y_val];
+    hline(line_h.YData(1));
+else
+    x_val = line_h.XData(1);
+    fh.UserData.x_slices = [fh.UserData.x_slices x_val];
+    vline(line_h.XData(1));
+end
+
+%Finds a double click
+if strcmpi(get(fh,'selectiontype'),'open')
+    uiresume(fh);
+end
+
+%Handle selection via enter/escape
+function handle_keys(~,event)
+   disp(event.Key);
+   switch event.Key
+       case {'return','escape'}
+           uiresume(gcf);
+   end
+
